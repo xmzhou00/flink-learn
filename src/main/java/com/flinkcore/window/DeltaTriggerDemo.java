ec425c2518958553724be7e6f30a97f5e942dc2e
Xianming Zhou <2024018084@qq.com>
2023-06-08 09:56:25 +0800

    update flink DeltaTrigger

--- src/main/java/com/flinkcore/window/DeltaTriggerDemo.java
+++ src/main/java/com/flinkcore/window/DeltaTriggerDemo.java
@@ -8,8 +8,11 @@ import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 import org.apache.flink.streaming.api.functions.windowing.ProcessWindowFunction;
 import org.apache.flink.streaming.api.functions.windowing.delta.DeltaFunction;
 import org.apache.flink.streaming.api.windowing.assigners.GlobalWindows;
+import org.apache.flink.streaming.api.windowing.evictors.Evictor;
 import org.apache.flink.streaming.api.windowing.triggers.DeltaTrigger;
+import org.apache.flink.streaming.api.windowing.triggers.PurgingTrigger;
 import org.apache.flink.streaming.api.windowing.windows.GlobalWindow;
+import org.apache.flink.streaming.runtime.operators.windowing.TimestampedValue;
 import org.apache.flink.util.Collector;
 
 import java.time.Duration;
@@ -24,13 +27,13 @@ public class DeltaTriggerDemo {
         final StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
         env.setParallelism(1);
         DataStreamSource<Tuple4<String, Integer, Integer, Integer>> carData = env.fromElements(
-                Tuple4.of("car1", 1, 1000, 111),
+                Tuple4.of("car1", 1, 999, 111),
                 Tuple4.of("car1", 2, 3000, 191),
                 Tuple4.of("car1", 3, 8000, 131),
-                Tuple4.of("car1", 4, 11001, 144),
-                Tuple4.of("car1", 5, 13000, 101),
+                Tuple4.of("car1", 4, 11000, 144),
+                Tuple4.of("car1", 5, 13000, 105),
                 Tuple4.of("car1", 6, 16000, 95),
-                Tuple4.of("car1", 7, 21000, 160),
+                Tuple4.of("car1", 7, 21001, 124),
                 Tuple4.of("car1", 8, 23000, 134));
 
         carData.assignTimestampsAndWatermarks(
@@ -44,7 +47,18 @@ public class DeltaTriggerDemo {
                                 })
                 ).keyBy(t -> t.f0)
                 .window(GlobalWindows.create())
-                .trigger(DeltaTrigger.of(10000, new DeltaFunction<Tuple4<String, Integer, Integer, Integer>>() {
+                .evictor(new Evictor<Tuple4<String, Integer, Integer, Integer>, GlobalWindow>() {
+                    @Override
+                    public void evictBefore(Iterable<TimestampedValue<Tuple4<String, Integer, Integer, Integer>>> elements, int size, GlobalWindow window, EvictorContext evictorContext) {
+                        System.out.println("evictor before");
+                    }
+
+                    @Override
+                    public void evictAfter(Iterable<TimestampedValue<Tuple4<String, Integer, Integer, Integer>>> elements, int size, GlobalWindow window, EvictorContext evictorContext) {
+                        System.out.println("evictor after");
+                    }
+                })
+                .trigger(PurgingTrigger.of(DeltaTrigger.of(10000, new DeltaFunction<Tuple4<String, Integer, Integer, Integer>>() {
                     @Override
                     public double getDelta(
                             Tuple4<String, Integer, Integer, Integer> oldDataPoint,
@@ -52,20 +66,24 @@ public class DeltaTriggerDemo {
                         System.err.println("oldDataPoint: " + oldDataPoint + " newDataPoint: " + newDataPoint);
                         return newDataPoint.f2 - oldDataPoint.f2;
                     }
-                }, carData.getType().createSerializer(env.getConfig())))
+                }, carData.getType().createSerializer(env.getConfig()))))
+//                        .max(3).print();
                 .process(new ProcessWindowFunction<Tuple4<String, Integer, Integer, Integer>, Object, String, GlobalWindow>() {
                     @Override
                     public void process(String s, ProcessWindowFunction<Tuple4<String, Integer, Integer, Integer>, Object, String, GlobalWindow>.Context context, Iterable<Tuple4<String, Integer, Integer, Integer>> elements, Collector<Object> out) throws Exception {
                         Iterator<Tuple4<String, Integer, Integer, Integer>> iterator = elements.iterator();
-                        int maxSpeed = 0;
+//                        int maxSpeed = 0;
                         while (iterator.hasNext()) {
                             Tuple4<String, Integer, Integer, Integer> next = iterator.next();
-                            maxSpeed = Math.max(maxSpeed, next.f3);
+                            System.out.println(next);
+//                            maxSpeed = Math.max(maxSpeed, next.f3);
                         }
-                        out.collect(maxSpeed);
+
+                        System.out.println("==============================");
+//                        out.collect(maxSpeed);
                     }
-                })
-                .print();
+                });
+//                .print();
 
         env.execute();
 
